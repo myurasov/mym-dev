34a310f64657e6fcb04c32a1de767351203c92e6
Mikhail Yurasov <me@yurasov.me>
2012-10-10 19:39:37 -0700

    Fix

--- src/mym/Helper/FirePHPHelper.php
+++ src/mym/Helper/FirePHPHelper.php
@@ -2,28 +2,10 @@
 
 namespace mym\Helper;
 
-use FirePHP;
 use mym\Kernel;
 
-class FirePHPHelper
-{
-  private static $firePhp;
+require_once \mym\Config::$options['libraries']['FirePHP'] . '/lib/FirePHPCore/fb.php';
 
-  /**
-   * Get FirePHP instance
-   *
-   * @return FirePHP
-   */
-  public static function getFirePhp()
-  {
-    if (\is_null(self::$firePhp))
-    {
-      require_once \mym\Config::$options['libraries']['FirePHP'] .
-        '/lib/FirePHPCore/FirePHP.class.php';
-
-      self::$firePhp = FirePHP::getInstance(true);
-    }
-
-    return self::$firePhp;
-  }
+class FirePHPHelper extends \FB {
+  //
 }
