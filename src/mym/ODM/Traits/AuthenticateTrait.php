9755b910f4812ee6e593b15878ab90808d484918
Mikhail Yurasov <me@yurasov.me>
2013-04-14 21:35:45 -0700

    AuthenticateTrait

---
+++ src/mym/ODM/Traits/AuthenticateTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace mym\ODM\Traits;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use mym\Exception\HttpForbiddenException;
+
+trait AuthenticateTrait
+{
+  /**
+   * Check if user is authenticated.
+   *
+   * @param Request $request
+   * @param bool $required
+   * @throws HttpForbiddenException
+   * @return User Current user
+   */
+  public static function authenticate(Request $request, $required = false)
+  {
+    $user = null;
+
+    // start session
+    $session = new Session();
+    $session->start();
+    $request->setSession($session);
+
+    if ($session->has("userId")) {
+      $user = self::load($session->get("userId"));
+    }
+
+    if ($user === null && $required) {
+      throw new HttpForbiddenException("User is not authenticated");
+    }
+
+    return $user;
+  }
+}
