c6c297670f04b99136ec1ecd29851563abb57013
Mikhail Yurasov <me@yurasov.me>
2013-10-20 13:12:37 -0700

    Default REST actions

--- src/mym/Component/REST/RESTControllerAbstract.php
+++ src/mym/Component/REST/RESTControllerAbstract.php
@@ -3,9 +3,161 @@
 namespace mym\Component\REST;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use mym\Exception\HttpNotImplementedException;
 
 class RESTControllerAbstract
 {
+  /**
+   * GET /collection/id/sub-collection/sub-id
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function getSubResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
 
+  /**
+   * PUT /collection/id/sub-collection/sub-id
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function updateOrCreateSubResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * DELETE /collection/id/sub-collection/sub-id
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function deleteSubResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * GET /collection/id/sub-collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function getSubCollectionAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * PUT /collection/id/sub-collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function replaceSubCollectionAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * POST /collection/id/sub-collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function createSubResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * DELETE /collection/id/sub-collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function deleteSubCollectionAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * GET /collection/id
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function getResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * PUT /collection/id
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function updateOrCreateResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * DELETE /collection/id
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function deleteResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * GET /collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function getCollectionAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * PUT /collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function replaceCollectionAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * POST /collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function createResourceAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
+
+  /**
+   * DELETE /collection
+   *
+   * @param Request $request
+   * @throws HttpNotImplementedException
+   */
+  public function deleteCollectionAction(Request $request)
+  {
+    throw new HttpNotImplementedException();
+  }
 }
