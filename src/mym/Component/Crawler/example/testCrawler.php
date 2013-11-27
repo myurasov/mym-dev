16f29564dd6bdb4a9aa4e698338325507c576a66
Mikhail Yurasov <me@yurasov.me>
2013-11-27 07:21:29 -0800

    Example

--- src/mym/Component/Crawler/example/testCrawler.php
+++ src/mym/Component/Crawler/example/testCrawler.php
@@ -4,6 +4,12 @@ require __DIR__ . '/../../src/modules/AppBase/Application.php';
 
 //
 
+$logger = new Monolog\Logger('crawler');
+$lh = new Monolog\Handler\StreamHandler('php://STDOUT', Monolog\Logger::DEBUG);
+$logger->pushHandler($lh);
+
+//
+
 $r = new mym\Component\Crawler\Repository\MongoRepository();
 $r->setMaxDepth(5);
 //$r->clear();
@@ -18,6 +24,7 @@ $pp = new mym\Component\Crawler\Processor\ProcessorPool();
 $pp->addProcessor(new \AppBase\Crawler\TestProcessor());
 
 $d = new \mym\Component\Crawler\NativeDispatcher();
+$d->setLogger($logger);
 $d->setRepository($r);
 $d->setProcessorPool($pp);
 
