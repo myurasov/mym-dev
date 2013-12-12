1f5005d608346a7cb109944b95d2630adc2c84ab
Mikhail Yurasov <me@yurasov.me>
2013-12-12 00:48:22 -0800

    ::createFromCode()

--- src/mym/Exception/HTTPException.php
+++ src/mym/Exception/HTTPException.php
@@ -11,5 +11,28 @@ use mym\Exception\Exception;
 
 class HTTPException extends Exception
 {
+  public static function createFromCode($code, $message = null)
+  {
+    if ($code === HttpBadRequestException::CODE) {
+      return is_null($message) ? new HttpBadRequestException() : new HttpBadRequestException($message);
+    }
 
+    if ($code === HttpForbiddenException::CODE) {
+      return is_null($message) ? new HttpForbiddenException() : new HttpForbiddenException($message);
+    }
+
+    if ($code === HttpMethodNotAllowedException::CODE) {
+      return is_null($message) ? new HttpMethodNotAllowedException() : new HttpMethodNotAllowedException($message);
+    }
+
+    if ($code === HttpNotFoundException::CODE) {
+      return is_null($message) ? new HttpNotFoundException() : new HttpNotFoundException($message);
+    }
+
+    if ($code === HttpNotImplementedException::CODE) {
+      return is_null($message) ? new HttpNotImplementedException() : new HttpNotImplementedException($message);
+    }
+
+    return is_null($message) ? new self($code) : new self($code, $message);
+  }
 }
