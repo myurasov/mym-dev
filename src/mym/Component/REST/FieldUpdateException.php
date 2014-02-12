242cd0c7165edb27c1f062929b5a226c45ecb353
Mikhail Yurasov <me@yurasov.me>
2014-02-12 03:22:08 -0800

    FieldUpdateException
    Raised when field cannot be updated

---
+++ src/mym/Component/REST/FieldUpdateException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Raised when field cannot be updated
+ * @copyright 2014 Mikhail Yurasov <me@yurasov.me>
+ */
+
+
+namespace mym\Component\REST;
+
+class FieldUpdateException extends \Exception
+{
+}
