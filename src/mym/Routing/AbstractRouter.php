cf9f55caaff4e14f58f24ceb7b7ddf0c85292306
Mikhail Yurasov <me@yurasov.me>
2014-02-22 16:29:42 -0800

    Routing refactoring

--- src/mym/Routing/AbstractRouter.php
+++ src/mym/Routing/AbstractRouter.php
@@ -5,13 +5,14 @@ namespace mym\Routing;
 use Symfony\Component\HttpFoundation\Request;
 use mym\Exception\HttpNotFoundException;
 
-class AbstractRouter implements RouterInterface
+abstract class AbstractRouter implements RouterInterface
 {
   protected $controller;
 
-  public function route(Request & $request)
-  {
-  }
+  /**
+   * @var UrlMatcherInterface[]
+   */
+  protected $urlMatchers = [];
 
   /**
    * @return callable
@@ -19,5 +20,26 @@ class AbstractRouter implements RouterInterface
   public function getController()
   {
     return $this->controller;
+  }
+
+  public function route(Request & $request)
+  {
+    // run through the list of url matchers
+    if (!$this->runMatchers($request)) {
+      throw new HttpNotFoundException();
+    }
+  }
+
+  protected function runMatchers(Request & $request)
+  {
+    for ($i = 0; $i < count($this->urlMatchers); $i++) {
+      $this->urlMatchers[$i]->match($request);
+
+      if ($this->controller = $this->urlMatchers[$i]->getController()) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
--- src/mym/Routing/DefaultRouter.php
+++ src/mym/Routing/DefaultRouter.php
@@ -7,34 +7,8 @@ use mym\Exception\HttpNotFoundException;
 
 class DefaultRouter extends AbstractRouter
 {
-  /**
-   * @var UrlMatcherInterface[]
-   */
-  protected $urlMatchers = [];
-
   public function __construct()
   {
     $this->urlMatchers[] = new DefaultUrlMatcher();
-  }
-
-  public function route(Request & $request)
-  {
-    // run through the list of url matchers
-    if (!$this->runMatchers($request)) {
-      throw new HttpNotFoundException();
-    }
-  }
-
-  private function runMatchers(Request & $request)
-  {
-    for ($i = 0; $i < count($this->urlMatchers); $i++) {
-      $this->urlMatchers[$i]->match($request);
-
-      if ($this->controller = $this->urlMatchers[$i]->getController()) {
-        return true;
-      }
-    }
-
-    return false;
   }
 }
